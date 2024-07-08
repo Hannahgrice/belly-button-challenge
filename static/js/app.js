39871f6c15ec210b0131a2d9e06bc77066a499d0
Hannahgrice <hannahgrice3194@icloud.com>
2024-07-08 13:52:24 -0500

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -9,18 +9,18 @@ function buildMetadata(sample) {
     let filteredMetadata = metadata.find(item => item.id === parseInt(sample));
 
     // Use d3 to select the panel with id of `#sample-metadata`
-    let panel = d3.select(“#sample - metadata”);
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-    panel.html(“”);
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
     Object.entries(filteredMetadata).forEach(([key, value]) => {
-      panel.append(“p”).text(`${key}: ${value}`);
+      panel.append("p").text(`${key}: ${value}`);
     });
   }).catch((error) => {
-    console.log(“Error loading JSON file:“, error);
+    console.log("Error loading JSON file:", error);
   });
 }
 
@@ -30,19 +30,19 @@ function buildCharts(sample) {
 
     // Get the samples field
     let samples = data.samples;
-    console.log(“Samples:“, samples);
+    console.log("Samples:", samples);
 
     // Filter the samples for the object with the desired sample number
     let filteredSample = samples.find(item => item.id === sample.toString());
-    console.log(“Filtered Sample:“, filteredSample);
+    console.log("Filtered Sample:", filteredSample);
 
     // Get the otu_ids, otu_labels, and sample_values
     let otu_ids = filteredSample.otu_ids;
     let otu_labels = filteredSample.otu_labels;
     let sample_values = filteredSample.sample_values;
-    console.log(“OTU IDs:“, otu_ids);
-    console.log(“OTU Labels:“, otu_labels);
-    console.log(“Sample Values:“, sample_values);
+    console.log("OTU IDs:", otu_ids);
+    console.log("OTU Labels:", otu_labels);
+    console.log("Sample Values:", sample_values);
 
 
     // Build a Bubble Chart
@@ -50,29 +50,29 @@ function buildCharts(sample) {
       x: otu_ids,
       y: sample_values,
       text: otu_labels,
-      mode: ‘markers’,
+      mode: "markers",
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: ‘Earth’
+        colorscale: "Earth"
       }
     };
     let dataBubble = [trace1];
     let layoutBubble = {
-      title: ‘OTU ID Bubble Chart’,
+      title: "OTU ID Bubble Chart",
       showlegend: false,
-      xaxis: { title: ‘OTU ID’ },
-      yaxis: { title: ‘Sample Values’ }
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Sample Values" }
     };
-    console.log(“Bubble Chart Data:“, dataBubble); // Log the bubble chart data
-    console.log(“Bubble Chart Layout:“, layoutBubble); // Log the bubble chart layout
-    Plotly.newPlot(‘bubble’, dataBubble, layoutBubble);
+    console.log("Bubble Chart Data:", dataBubble); // Log the bubble chart data
+    console.log("Bubble Chart Layout:", layoutBubble); // Log the bubble chart layout
+    Plotly.newPlot("bubble", dataBubble, layoutBubble);
     buildCharts(940);
 
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
     let yticks = otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse();
-    console.log(“YTicks:“, yticks);
+    console.log("YTicks:", yticks);
 
     // Build a Bar Chart
 
@@ -81,28 +81,26 @@ function buildCharts(sample) {
       x: sample_values.slice(0, 10).reverse(),
       y: yticks,
       text: otu_labels.slice(0, 10).reverse(),
-      type: ‘bar’,
-      orientation: ‘h’
+      type: "bar",
+      orientation: "h"
     };
 
 
     // Render the Bar Chart
     let dataBar = [trace2];
     let layoutBar = {
-      title: ‘Top 10 OTUs Found’,
-      xaxis: { title: ‘Sample Values’ },
-      yaxis: { title: ‘OTU ID’ }
+      title: "Top 10 OTUs Found",
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU ID" }
     };
-    console.log(“Bar Chart Data:“, dataBar); // Log the bar chart data
-    console.log(“Bar Chart Layout:“, layoutBar); // Log the bar chart layout
-    Plotly.newPlot(‘bar’, dataBar, layoutBar);
+    console.log("Bar Chart Data:", dataBar); // Log the bar chart data
+    console.log("Bar Chart Layout:", layoutBar); // Log the bar chart layout
+    Plotly.newPlot("bar", dataBar, layoutBar);
   }).catch((error) => {
-    console.log(“Error loading JSON file:“, error);
+    console.log("Error loading JSON file:", error);
   });
 }
 buildCharts(940);
-      });
-    }
 
 // Function to run on page load
 function init() {
@@ -112,13 +110,13 @@ function init() {
     let names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-    let dropdown = d3.select(“#selDataset”);
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
     names.forEach((sample) => {
-      dropdown.append(“option”).text(sample).property(“value”, sample);
+      dropdown.append("option").text(sample).property("value", sample);
     });
 
     // Get the first sample from the list
@@ -128,7 +126,7 @@ function init() {
     buildCharts(firstSample);
     buildMetadata(firstSample);
   }).catch((error) => {
-    console.log(“Error loading JSON file:“, error);
+    console.log("Error loading JSON file:", error);
   });
 }
 
@@ -141,3 +139,4 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
